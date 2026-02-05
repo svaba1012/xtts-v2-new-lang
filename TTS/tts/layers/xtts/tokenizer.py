2f90f94df1d5883dcc78a328c21a8f3e410e71f6
svaba <stefanivanovic1012@gmail.com>
2026-02-05 10:16:38 +0100

    Extending embeddings

--- TTS/tts/layers/xtts/tokenizer.py
+++ TTS/tts/layers/xtts/tokenizer.py
@@ -641,7 +641,7 @@ class VoiceBpeTokenizer:
             )
 
     def preprocess_text(self, txt, lang):
-        if lang in {"ar", "cs", "de", "en", "es", "fr", "hi", "hu", "it", "nl", "pl", "pt", "ru", "tr", "zh", "ko"}:
+        if lang in {"ar", "cs", "de", "en", "es", "fr", "hi", "hu", "it", "nl", "pl", "pt", "ru", "tr", "zh", "ko", "sr"}:
             txt = multilingual_cleaners(txt, lang)
             if lang == "zh":
                 txt = chinese_transliterate(txt)
