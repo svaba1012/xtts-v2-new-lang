ee896cff374a1e38bdddea9d5b3c84f9b7b6a4be
svaba <stefanivanovic1012@gmail.com>
2026-02-05 09:58:57 +0100

    Extending embeddings

--- extend_vocab_config.py
+++ extend_vocab_config.py
@@ -99,19 +99,19 @@ def adjust_embeddings():
     old_emb = state_dict["gpt.text_embedding.weight"]
     new_vocab_size = 8464  # your new vocab
     new_emb = torch.nn.Parameter(torch.randn(new_vocab_size, old_emb.size(1)))  # random init
-    new_emb[:old_emb.size(0), :] = old_emb.data
+    new_emb[:old_emb.size(0), :] = old_emb.clone()
     state_dict["gpt.text_embedding.weight"] = new_emb
 
     # Text head
     old_head = state_dict["gpt.text_head.weight"]
     new_head = torch.nn.Parameter(torch.randn(new_vocab_size, old_head.size(1)))
-    new_head[:old_head.size(0), :] = old_head.data
+    new_head[:old_head.size(0), :] = old_head.clone()
     state_dict["gpt.text_head.weight"] = new_head
 
     # Text head bias
     old_bias = state_dict["gpt.text_head.bias"]
     new_bias = torch.nn.Parameter(torch.zeros(new_vocab_size))
-    new_bias[:old_bias.size(0)] = old_bias.data
+    new_bias[:old_bias.size(0)] = old_bias.clone()
     state_dict["gpt.text_head.bias"] = new_bias
 
     # Save back
