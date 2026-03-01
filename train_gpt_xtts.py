a95f3d0881f22efdf96524e5f2a17c9a28bc2ee8
Stefan Ivanović <stefanivanovic1012@gmail.com>
2026-03-02 00:49:58 +0100

    Remove save_on_epoch from configuration
    Removed the save_on_epoch configuration option.

--- train_gpt_xtts.py
+++ train_gpt_xtts.py
@@ -170,7 +170,6 @@ def train_gpt(metadatas, num_epochs, batch_size, grad_acumm, output_path, max_au
     config.save_step = save_step
     config.save_n_checkpoints = 1
     config.save_checkpoints = True
-    config.save_on_epoch = False
     config.print_eval = False
     config.optimizer = "AdamW"
     config.optimizer_wd_only_on_weights = OPTIMIZER_WD_ONLY_ON_WEIGHTS
